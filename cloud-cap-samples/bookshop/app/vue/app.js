f610e5799cac014b95d6fda63cd69159cb8186f2
I335851 <petr.hanak@sap.com>
2023-08-21 18:55:47 +0200

    add modified examples with V2 proxy

---
+++ cloud-cap-samples/bookshop/app/vue/app.js
@@ -0,0 +1,48 @@
+/* global Vue axios */ //> from vue.html
+const $ = sel => document.querySelector(sel)
+const GET = (url) => axios.get('/browse'+url)
+const POST = (cmd,data) => axios.post('/browse'+cmd,data)
+
+const books = Vue.createApp ({
+
+    data() {
+      return {
+        list: [],
+        book: undefined,
+        order: { quantity:1, succeeded:'', failed:'' }
+      }
+    },
+
+    methods: {
+
+        search: ({target:{value:v}}) => books.fetch(v && '&$search='+v),
+
+        async fetch (etc='') {
+            const {data} = await GET(`/ListOfBooks?$expand=genre,currency${etc}`)
+            books.list = data.value
+        },
+
+        async inspect (eve) {
+            const book = books.book = books.list [eve.currentTarget.rowIndex-1]
+            const res = await GET(`/Books/${book.ID}?$select=descr,stock,image`)
+            Object.assign (book, res.data)
+            books.order = { quantity:1 }
+            setTimeout (()=> $('form > input').focus(), 111)
+        },
+
+        async submitOrder () {
+            const {book,order} = books, quantity = parseInt (order.quantity) || 1 // REVISIT: Okra should be less strict
+            try {
+                const res = await POST(`/submitOrder`, { quantity, book: book.ID })
+                book.stock = res.data.stock
+                books.order = { quantity, succeeded: `Successfully ordered ${quantity} item(s).` }
+            } catch (e) {
+                books.order = { quantity, failed: e.response.data.error.message }
+            }
+        }
+
+    }
+}).mount("#app")
+
+// initially fill list of books
+books.fetch()
